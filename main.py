a44c6b34a0c5faa88da449f6d2a3f881d1bb13a4
Marshal10 <marshalmaleane123@gmail.com>
2024-01-02 21:06:13 +0530

    Get random Quote

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import smtplib
 import datetime as dt
+import random
 now=dt.datetime.now()
 weekday=now.weekday()
 
-print(weekday)
+if weekday==0:
+    with open("quotes.txt") as file:
+        quotes=file.readlines()
+    random_quote=random.choice(quotes)
+    print(random_quote)
